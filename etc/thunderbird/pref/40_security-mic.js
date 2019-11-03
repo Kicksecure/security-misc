b55c2fd62e200f96bd552445ad4c517d6a0aee92
Patrick Schleizer <adrelanos@riseup.net>
2019-11-03 02:50:51 -0500

    Enables punycode (`network.IDN_show_punycode`) by default in Thunderbird to make phising attacks more difficult. Fixing URL not showing real Domain Name (Homograph attack).
    https://forums.whonix.org/t/enable-network-idn-show-punycode-by-default-in-thunderbird-to-fix-url-not-showing-real-domain-name-homograph-attack-punycode/8415

---
+++ etc/thunderbird/pref/40_security-mic.js
@@ -0,0 +1,8 @@
+//#### meta start
+//#### project Whonix and Kicksecure
+//#### category security and apps
+//#### description https://forums.whonix.org/t/enable-network-idn-show-punycode-by-default-in-thunderbird-to-fix-url-not-showing-real-domain-name-homograph-attack-punycode/8415
+//#### meta end
+
+// https://forums.whonix.org/t/enable-network-idn-show-punycode-by-default-in-thunderbird-to-fix-url-not-showing-real-domain-name-homograph-attack-punycode/8415
+pref("network.IDN_show_punycode", true);
