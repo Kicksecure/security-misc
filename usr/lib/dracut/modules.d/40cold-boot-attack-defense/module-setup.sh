148a050468658c254b67de2de61cad3e147e2178
Patrick Schleizer <adrelanos@whonix.org>
2022-07-02 16:03:45 -0400

    fix

--- usr/lib/dracut/modules.d/40cold-boot-attack-defense/module-setup.sh
+++ usr/lib/dracut/modules.d/40cold-boot-attack-defense/module-setup.sh
@@ -14,6 +14,7 @@ check() {
     require_binaries sleep || return 1
     require_binaries dmsetup || return 1
     require_binaries sdmem || return 1
+    require_binaries systemd-detect-virt || return 1
     return 0
 }
 
@@ -27,6 +28,7 @@ install() {
     inst_multiple sleep
     inst_multiple sdmem
     inst_multiple dmsetup
+    inst_multiple systemd-detect-virt
     inst_hook shutdown 40 "$moddir/wipe-ram.sh"
     inst_hook cleanup 80 "$moddir/wipe-ram-needshutdown.sh"
 }
