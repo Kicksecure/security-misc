779ad24b573b83c08e89569e5213e018377d1535
Friedrich Doku <frd20@pitt.edu>
2023-01-06 13:53:18 -0500

    Update wipe-ram-needshutdown.sh

--- usr/lib/dracut/modules.d/40cold-boot-attack-defense/wipe-ram-needshutdown.sh
+++ usr/lib/dracut/modules.d/40cold-boot-attack-defense/wipe-ram-needshutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-## Copyrigh (C) 2023 - 2023 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
-## Copyrigh (C) 2023 - 2023 Friedrich Doku <friedrichdoku@gmail.com>
+## Copyright (C) 2023 - 2023 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
+## Copyright (C) 2023 - 2023 Friedrich Doku <friedrichdoku@gmail.com>
 ## See the file COPYING for copying conditions.
 
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
