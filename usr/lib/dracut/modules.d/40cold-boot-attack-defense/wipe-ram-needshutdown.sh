ab89d0e06e68fa47fa4058416a6c8700551f1b9a
Patrick Schleizer <adrelanos@whonix.org>
2023-01-07 16:59:00 -0500

    cleanup

--- usr/lib/dracut/modules.d/40cold-boot-attack-defense/wipe-ram-needshutdown.sh
+++ usr/lib/dracut/modules.d/40cold-boot-attack-defense/wipe-ram-needshutdown.sh
@@ -7,16 +7,11 @@
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 ram_wipe_check_needshutdown() {
-   local OLD_DRACUT_QUIET
-   OLD_DRACUT_QUIET="$DRACUT_QUIET"
-   DRACUT_QUIET='no'
-
    local kernel_wiperam_setting
    kernel_wiperam_setting=$(getarg wiperam)
 
    if [ "$kernel_wiperam_setting" = "skip" ]; then
       info "wipe-ram-needshutdown.sh: Skip, because wiperam=skip kernel parameter detected, OK."
-      DRACUT_QUIET="$OLD_DRACUT_QUIET"
       return 0
    fi
 
@@ -29,7 +24,6 @@ ram_wipe_check_needshutdown() {
       info "wipe-ram-needshutdown.sh: detect_virt_exit_code: '$detect_virt_exit_code'"
       if [ "$detect_virt_exit_code" = "0" ]; then
          info "wipe-ram-needshutdown.sh: Skip, because running inside a VM detected and not using wiperam=force kernel parameter, OK."
-         DRACUT_QUIET="$OLD_DRACUT_QUIET"
          return 0
       fi
       info "wipe-ram-needshutdown.sh: Bare metal (not running inside a VM) detected, OK."
@@ -38,7 +32,6 @@ ram_wipe_check_needshutdown() {
    info "wipe-ram-needshutdown.sh: Calling dracut function need_shutdown to drop back into initramfs at shutdown, OK."
    need_shutdown
 
-   DRACUT_QUIET="$OLD_DRACUT_QUIET"
    return 0
 }
 
