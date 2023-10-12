fa820e897895eda93011a0f2bbd915ffffcb1459
Patrick Schleizer <adrelanos@whonix.org>
2023-10-12 10:40:27 -0400

    refactoring environment variables loading mechanism

--- etc/profile.d/security-misc.sh
+++ etc/profile.d/security-misc.sh
@@ -3,6 +3,11 @@
 ## Copyright (C) 2019 - 2023 ENCRYPTED SUPPORT LP <adrelanos@whonix.org>
 ## See the file COPYING for copying conditions.
 
+if [ -z "$XDG_CONFIG_DIRS" ]; then
+   XDG_CONFIG_DIRS=/etc/xdg
+fi
+export XDG_CONFIG_DIRS=/usr/share/security-misc/:$XDG_CONFIG_DIRS
+
 if [ -x /usr/libexec/security-misc/panic-on-oops ]; then
    sudo --non-interactive /usr/libexec/security-misc/panic-on-oops
 fi
