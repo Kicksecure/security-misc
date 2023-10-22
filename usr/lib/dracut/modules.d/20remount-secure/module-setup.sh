90f2b5e11c341c38bb0b11db603ceeba28e14b1c
Patrick Schleizer <adrelanos@whonix.org>
2023-10-22 08:51:37 -0400

    code simplification

--- usr/lib/dracut/modules.d/20remount-secure/module-setup.sh
+++ usr/lib/dracut/modules.d/20remount-secure/module-setup.sh
@@ -5,7 +5,6 @@
 
 # called by dracut
 check() {
-    require_binaries str_replace || return 1
     require_binaries mount || return 1
     require_binaries remount-secure || return 1
     return 0
@@ -18,7 +17,6 @@ depends() {
 
 # called by dracut
 install() {
-    inst_multiple str_replace
     inst_multiple mount
     inst_multiple remount-secure
     inst_hook cleanup 90 "$moddir/remount-secure.sh"
