6b76373395622bac0e701c6d15c6656658febced
Patrick Schleizer <adrelanos@whonix.org>
2024-03-04 06:44:26 -0500

    fix panic-on-oops started every 10s in Qubes-Whonix
    by changing from a /etc/profile.d etc. related mechanism to start to a systemd unit file based approach
    
    Thanks to @marmarek for the bug report!
    
    https://forums.whonix.org/t/panic-on-oops-started-every-10s/19450

--- etc/profile.d/30_security-misc.sh
+++ etc/profile.d/30_security-misc.sh
@@ -9,8 +9,3 @@ fi
 if ! echo "$XDG_CONFIG_DIRS" | grep --quiet /usr/share/security-misc/ ; then
    export XDG_CONFIG_DIRS=/usr/share/security-misc/:$XDG_CONFIG_DIRS
 fi
-
-if [ -x /usr/libexec/security-misc/panic-on-oops ]; then
-   ## Hide output. Otherwise could confuse Qubes UpdatesProxy.
-   sudo --non-interactive /usr/libexec/security-misc/panic-on-oops 1>/dev/null 2>/dev/null
-fi
